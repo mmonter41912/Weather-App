4316cb63aee7d9d74b6b753b274f015da5444305
mmonter41912 <montermarlen@gmail.com>
2020-10-28 15:40:15 -0700

    Updated HTML, CSS, JS
    HTML : Moved the button location arrow into <nav>. Replace "images/sun.svg" with "images/01d@2xpng." Moved the open source link outside of the container.
    
    CSS: Increased the border radius for the container. Stylized the search and location buttons.
    
    JS: Weather description is now working.

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
+function formatDate(timestamp) {
+  let currentDate = new Date(timestamp);
 
+  let hour = currentDate.getHours();
+  let minutes = currentDate.getMinutes();
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+  let day = days[currentDate.getDay()];
+
+  return `${day} ${hour}:${minutes}`;
+}
+
+let now = new Date();
 let h2 = document.querySelector("h2");
-
-let currentDate = new Date();
-
-let hour = currentDate.getHours();
-let minutes = currentDate.getMinutes();
-
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let day = days[currentDate.getDay()];
-
-h2.innerHTML = `${day} ${hour}:${minutes}`;
-
-
+h2.innerHTML = formatDate(now);
 
 function searchCity(city) {
   let apiKey = "f7bcaae56975747b83d88801e136e5f9";
@@ -53,27 +54,24 @@ searchForm.addEventListener("submit", handleSubmit);
 let currentLocation = document.querySelector("#currentLocationButton");
 currentLocation.addEventListener("click", getCurrentLocation);
 
-
-
 function displayWeatherCondition(response) {
-let temperatureElement = document.querySelector("#tempNumber");
-let cityElement = document.querySelector("#city");
-let descriptionElement = document.querySelector ("#description ");
-let humidityElement = document.querySelector("#humidity");
-let windElement = document.querySelector("#wind");
-let dateElement = document.querySelector("#date");
+  let temperatureElement = document.querySelector("#tempNumber");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector(".description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("h2");
+  let iconElement = document.querySelector("#icon");
 
-temperatureElement.innerHTML = Math.round(response.data.main.temp);
-cityElement.innerHTML = response.data.name;
-descriptionElement.innerHTML = response.data.weather[0].description;
-humidityElement.innerHTML = response.data.main.humidity;
-windElement.innerHTML = Math.round(response.data.wind.speed);
-dateElement.innerHTML = formatDate(response.data.dt * 1000);
-
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
-
-
-
 
 let apiKey = "f7bcaae56975747b83d88801e136e5f9";
 let city = "San Francisco";
